c97b3120c56076c0ec76c54a5ebaa5e05bd81f86
Lucas Tannuri <lucas.tannuri1808@gmail.com>
2026-09-21 14:05:25 -0300

    feat: cria classe Especie
    - Adiciona cadastro de espécies
    4
    - Implementa busca por nome e UUID
    5
    - Evita espécies duplicadas
    6
    - Adiciona persistência em JSON
    7
    - Integra geração de identificadores UUID

---
+++ Classes/especies.py
@@ -0,0 +1,40 @@
+import uuid
+from Data.json_storage import carregar, salvar
+class Especie():
+
+  def cadastrar_especie(self, nome, tratamento, banho_de_sol):
+      lista_de_especies = carregar("Data/especies.json") or []
+      especie = self.verificar_especie(nome=nome)
+
+      if especie:
+        print(f"Espécie {especie['nome']} já catalogada!")
+
+      else:
+        nova_especie = {
+          "id": str(uuid.uuid4()),
+          "nome": nome.strip().lower(),
+          "tratamento": tratamento,
+          "banho_de_sol": banho_de_sol
+        }
+        lista_de_especies.append(nova_especie)
+        salvar(dado=lista_de_especies, local="Data/especies.json", identacao=4)
+        return False
+
+  def verificar_especie(self, nome=None, id_especie=None):
+    if nome is None and id_especie is None:
+      raise ValueError("Informe nome ou id_especie.")
+
+    lista_de_especies = carregar("Data/especies.json") or []
+    if nome:
+      for especie in lista_de_especies:
+        if especie["nome"].strip.lower() == nome.lower():
+          return especie
+    if id_especie:
+      for especie in lista_de_especies:
+              if especie["id"] == id_especie:
+                return especie
+
+
+    return None
+  
+    
