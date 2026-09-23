281605b302c07fda3b31716c3099f56a793c304a
Lucas Tannuri <lucas.tannuri1808@gmail.com>
2026-09-23 11:14:04 -0300

    refactor: adapta cronograma para uso de UUIDs
    - Substitui referências diretas a animais e recintos por UUIDs
    - Atualiza classe Tarefa para armazenar id_animal e id_recinto
    - Adequa serialização e desserialização do cronograma
    - Atualiza execução de tarefas no Tratador
    - Padroniza relacionamento entre entidades do sistema

--- Models/tratador.py
+++ Models/tratador.py
@@ -1,79 +1,26 @@
 #Importacoes
-from enum import Enum
-from Data.json_storage import salvar, carregar
+from cronograma import Cronograma
+from tarefa import TipoTarefa
 
-
-#Classe de estado
-class TipoTarefa(Enum):
-  TRATAR = "Tratar"
-  ALIMENTAR = "Alimentar"
-  LIMPAR = "Limpar" 
-  BANHO = "Banho"
-
-
-#--------------------------
-#Classe Tarefa
-class Tarefa():
-  def __init__(self, horario,tipo, animal=None, recinto=None):
-    self.horario = horario
-    self.tipo = tipo
-    self.animal = animal
-    self.recinto = recinto
-
-#Classe Cronograma
-class Cronograma():
-  
-
-  def __init__(self):
-    self.cronograma = carregar(r"Data\cronograma.json") or []
-
-
-  def adicionar_tarefa(self, tarefa):
-    for t in self.cronograma:
-      if t["horario"] == tarefa.horario:
-        return "Horário já ocupado."
-
-    nova_tarefa = {
-      "horario": tarefa.horario,
-      "tipo": tarefa.tipo.value,
-      "animal": tarefa.animal.nome if tarefa.animal else None,
-      "recinto": tarefa.recinto.nome if tarefa.recinto else None
-    }
-
-    self.cronograma.append(nova_tarefa)
-    salvar(dado=self.cronograma, local=r"Data\cronograma.json", identacao=4)
-
-
-  def buscar_tarefa(self, horario):
-    for t in self.cronograma:
-      if t["horario"] == horario:
-        return Tarefa(horario=t["horario"], tipo=TipoTarefa(t["tipo"]), animal=t["animal"], recinto=t["recinto"])
-
-    
-          
-
-#--------------------------
-#Classe Tratador
 class Tratador():
   def __init__(self, nome):
     self.nome = nome
     self.cronograma = Cronograma()
 
 
-  
-  def tratar_animal(self, animal):
+  def tratar_animal(self, id_animal):
     pass
 
 
-  def alimentar_animal(self, animal):
+  def alimentar_animal(self, id_animal):
     pass
 
 
-  def limpar_recinto(self, recinto):
+  def limpar_recinto(self, id_recinto):
     pass
 
 
-  def banho_de_sol(self, animal):
+  def banho_de_sol(self, id_animal):
     pass
 
 
@@ -81,21 +28,21 @@ class Tratador():
     match tarefa.tipo:
           
           case TipoTarefa.TRATAR:
-            self.tratar_animal(tarefa.animal)
+            self.tratar_animal(tarefa.id_animal)
             
     
           
           case TipoTarefa.ALIMENTAR:
-            self.alimentar_animal(tarefa.animal)
+            self.alimentar_animal(tarefa.id_animal)
             
     
           case TipoTarefa.LIMPAR:
-            self.limpar_recinto(tarefa.recinto)
+            self.limpar_recinto(tarefa.id_recinto)
             
     
     
           case TipoTarefa.BANHO:
-            self.banho_de_sol(tarefa.animal)
+            self.banho_de_sol(tarefa.id_animal)
           
 
           case _:
